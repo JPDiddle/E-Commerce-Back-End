fc7e56cc0340c55796e52b6777458e41edc6dafd
Jack Price <jackprice1000@gmail.com>
2021-11-07 19:50:18 -0700

    commit

---
+++ routes/api/category-routes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include:[{model: Product}]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findOne({
+      where:{
+        id: req.params.id
+      },
+      include:[{model: Product}]
+    })
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try{
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+
+  }catch(err){
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where:{
+        id: req.params.id
+      },  include:[{model: Product}]
+    })
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where:{
+        id: req.params.id
+      }
+    })
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
